8ece5913525dcbc1c7bddb640b6a84a703f1b519
benjamingohps <benjamingohps@gmail.com>
2024-09-30 11:38:09 +0800

    when numbers are clicked, they are displayed in the calculator display

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll(".button");
+
+const calculator = document.querySelector("#calculator");
+const display = document.querySelector(".display");
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+// Using forEach to assign event listeners
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     // This log is for testing purposes to verify we're getting the correct value
+//     console.log(event.target.innerText);
+//     // Future logic to capture the button's value would go here...
+//   });
+// });
+
+// Using event delegation
+calculator.addEventListener("click", (event) => {
+  // This log is for testing purposes to verify we're getting the correct value
+  // You have to click a button to see this log
+  console.log(event.target.innerText);
+  display.innerText += event.target.innerText;
+
+  // Example
+  if (event.target.classList.contains("number")) {
+    // Do something with a number
+  }
+
+  // Example
+  if (event.target.innerText === "*") {
+    // Do something with this operator
+  }
+});
 
 /*-------------------------------- Functions --------------------------------*/
